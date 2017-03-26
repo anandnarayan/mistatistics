f5a20a92987f9c3850f338b7eb3c9d6f2e6c29ea
anandnarayan <anandnarayan@live.com>
2017-03-26 22:10:02 +0530

    MS-A in progress

--- Code/src/com/company/InputFileReader.java
+++ Code/src/com/company/InputFileReader.java
@@ -3,35 +3,57 @@ package com.company;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.stream.Stream;
+import java.util.ArrayList;
 
 /**
  * Created by anand on 3/22/2017.
  */
 public class InputFileReader {
+    static long TransactionID = 1;
+    static HashMap<Long,Long> itemVsCountHashMap = new HashMap<Long,Long>();
 
+    public static HashMap<Long,ArrayList<Long>> GetFileData(String filename) {
+        HashMap<Long,ArrayList<Long>> TransactionMap = new HashMap<Long,ArrayList<Long>>();
 
-    public static HashMap<Long,Long> GetFileData(String filename) {
-        HashMap<Long,Long> TransactionMap = new HashMap<Long,Long>();
-        long TransactionID = 1;
         try(Stream<String> stream = Files.lines(Paths.get(filename)))
         {
-            stream.forEach((str,Trans)->{
+            stream.forEach((str)->{
                 System.out.println(str);
                 if(!str.isEmpty())
                 {
                     String transactionItemsStrings[] = str.split(" ");
+                    ArrayList<Long> itemList = null;
                     for (String transaction: transactionItemsStrings)
                     {
-                    TransactionMap.put(TransactionID,Long.parseLong(transaction));
-                        TransactionID++;
+                        itemList = new ArrayList<Long>();
+                        Long item = Long.parseLong(transaction);
+                        itemList.add(item);
+                        if(itemVsCountHashMap.containsKey(item))
+                        {
+                            Long aLong = itemVsCountHashMap.get(item);
+                            itemVsCountHashMap.put(item,++aLong);
+                        }
+                        else
+                        {
+                            itemVsCountHashMap.put(item, (long) 1);
+                        }
+                    }
+
+                    if(!itemList.isEmpty())
+                    {
+                        Collections.sort(itemList);
+                        TransactionMap.put(TransactionID,itemList);
+                        ++TransactionID;
                     }
                 }
             });
         } catch (IOException e) {
             e.printStackTrace();
         }
+        TransactionID = 0;
         return TransactionMap;
     }
 }
--- Code/src/com/company/Main.java
+++ Code/src/com/company/Main.java
@@ -1,17 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.company.InputFileReader.GetFileData;
 
 public class Main {
+    static double minSupport = .5;
+    static double minConfidence = .5;
+    static  HashMap<Long,ArrayList<Long>> FileData = null;
+    static double delta = .75;
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("testing1");
         //Read the input file
         String filename = "F:\\EMDS\\Analytical Databases\\assignments\\Assign2\\retail1.txt";
-        HashMap<Long,Long> FileData = GetFileData(filename);
+        FileData = GetFileData(filename);
+        //calculate minimum support for each item
+        //min item support =>
+        //Generate and Sort Minimum Support
+        HashMap<Long,Double> itemVsInitialSupport = new HashMap<Long,Double>();
+        InputFileReader.itemVsCountHashMap.forEach((k,v)->{
+            itemVsInitialSupport.put(k,(v/FileData.size())* delta);
+        });
+        Stream<Map.Entry<Long,Double>> sorted = itemVsInitialSupport.entrySet().stream().sorted(Map.Entry.comparingByValue());
+
     }
 
 
